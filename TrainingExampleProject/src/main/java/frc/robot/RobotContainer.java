ae3d8f6057b2791e23cd27520a4aeb5d24fe47a5
Oliver Weller <ImpactTang@outlook.com>
2024-11-14 00:05:21 -0600

    Removed Test Comment

--- TrainingExampleProject/src/main/java/frc/robot/RobotContainer.java
+++ TrainingExampleProject/src/main/java/frc/robot/RobotContainer.java
@@ -9,13 +9,6 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.Intake;
 
-/**
- * This class is where the bulk of the robot should be declared. Since
- * Command-based is a "declarative" paradigm, very little robot logic should
- * actually be handled in the {@link Robot} periodic methods (other than the
- * scheduler calls). Instead, the structure of the robot (including subsystems,
- * commands, and button mappings) should be declared here.
- */
 public class RobotContainer {
 
   private final Intake intake;
