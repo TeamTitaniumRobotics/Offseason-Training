62efa0e8fb8295793f69edcac2fb54640e0c89b3
Oliver W <ImpactTang@outlook.com>
2024-11-25 19:27:19 -0600

    Renamed Swerve Subsystem and Added Base for Auto

--- Scream2024ExampleProject/src/main/java/frc/robot/commands/AutoRoutines.java
+++ Scream2024ExampleProject/src/main/java/frc/robot/commands/AutoRoutines.java
@@ -1,5 +1,9 @@
 package frc.robot.commands;
 
+import frc.robot.subsystems.drivetrain.Swerve;
+
 public class AutoRoutines {
-    
+    public AutoRoutines(Swerve drivetrain) {
+        // Add your auto routines here
+    }
 }
